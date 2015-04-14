a263c71019272b7d478ac0132a08da51363e5bd3
boathouse2112 <boathouse2112@gmail.com>
2015-04-14 06:34:46 -0400

    not sure

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -15,17 +16,31 @@ class NewVisitorTest(unittest.TestCase):
     # He goes online to find a to-do app.
     self.browser.get("http://localhost:8000")
 
-    # He looks for "To-Do" in the title, otherwise how would he know what the site is?
+    # He looks for "To-Do" in the title and header, otherwise how would he know what the site is?
     self.assertIn("To-Do", self.browser.title)
-    self.fail("Finish the test!")
+    header_text = self.browser.find_element_by_tag_name('h1').text
+    self.assertIn('To-Do', header_text)
 
     # The site tells him to enter a thing, he does.
+    inputbox = self.browser.find_element_by_id('id_new_item')
+    self.assertEqual(
+      inputbox.get_attribute('placeholder'),
+      'Enter a to-do item'
+    )
 
     # He types "Learn to read" into the prompt
+    inputbox.send_keys('Learn to read')
+    inputbox.send_keys(Keys.ENTER)
 
     # The page updates, and he sees some text he doesn't know how to read.
+    table = self.browser.find_element_by_id('id_list_table')
+    rows = table.find_element_by_tag_name('tr')
+    self.assertTrue(
+      any(row.text == '1: Learn to read' for row in rows)
+    )
 
     # He sees a box for more typing. He enters "Learn Math"
+    self.fail('Finish the test!')
 
     # Suddenly, there are TWO texts he can't read.
 
